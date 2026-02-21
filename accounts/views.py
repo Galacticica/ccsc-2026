0c98f138d383c0c004aee3fd60e70c38ee750353
Galacticica <ojthecat127@gmail.com>
2026-02-20 18:17:31 -0600

    feat: initialize project with Vite and Django integration
    - Added package.json for frontend dependencies and scripts using Vite.
    - Created pyproject.toml for backend dependencies including Django and related packages.
    - Introduced app.css and app.js for styling and JavaScript functionality with Tailwind CSS.
    - Implemented base.html template with HTMX and Trix editor integration.
    - Generated uv.lock for dependency management.
    - Configured Vite with a vite.config.js file for building assets.

---
+++ accounts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import redirect
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from django.contrib.auth import login, logout
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.views import View
+from .forms import LoginForm, SignupForm
+
+
+User = get_user_model()
+
+class MyLoginView(LoginView):
+    """Custom login view using MyLoginForm."""
+    form_class = LoginForm
+    template_name = "accounts/login.html"
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return "/"
+
+class MySignupView(FormView):
+    """Custom signup view using SignupForm."""
+    form_class = SignupForm
+    template_name = "accounts/signup.html"
+    success_url = "/" 
+
+    def form_valid(self, form):
+        user = User.objects.create(
+            email=form.cleaned_data["email"],
+            first_name=form.cleaned_data["first_name"],
+            last_name=form.cleaned_data["last_name"],
+            password=make_password(form.cleaned_data["password"]), 
+        )
+        login(self.request, user)
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+class MyLogoutView(View):
+    """Custom logout view."""
+    def get(self, request, *args, **kwargs):
+        logout(request)  
+        return redirect("/")  
+
