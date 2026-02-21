9758b6c71dcc1f2d55baea63f719559d805c9882
Galacticica <ojthecat127@gmail.com>
2026-02-20 23:13:20 -0600

    feat: register AIModel and AIQuirk in admin with inline support

--- ai_models/admin.py
+++ ai_models/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import AIModel, AIQuirk
 
-# Register your models here.
+
+class AIQuirkInline(admin.TabularInline):
+    model = AIModel.quirk.through
+    extra = 1
+
+
+@admin.register(AIModel)
+class AIModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines = [AIQuirkInline]
+
+
+@admin.register(AIQuirk)
+class AIQuirkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
